c7001041dec8dd88fde0ba7559f83dd6ca043fe5
vaccinatedProgrammer <bharat.suryawanshi@koodoo.io>
2023-02-11 19:50:11 -0500

    Revert "run dynamodb and serveless locally"
    This reverts commit aac03d4f9f99c294b63b646a98491e27313d52f0.

--- db.js
+++ db.js
@@ -1,11 +1,6 @@
 // instantiate dynamoDB client
 
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const client = new DynamoDBClient({
-  region: "us-west-2",
-  accessKeyId: "access_key_id",
-  secretAccessKeyId: "secret_access_key_id",
-  endpoint: "http://localhost:8000",
-});
+const client = new DynamoDBClient({});
 
 module.exports = client;
