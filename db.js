aac03d4f9f99c294b63b646a98491e27313d52f0
vaccinatedProgrammer <bharat.suryawanshi@koodoo.io>
2023-02-11 19:30:40 -0500

    run dynamodb and serveless locally

--- db.js
+++ db.js
@@ -1,6 +1,11 @@
 // instantiate dynamoDB client
 
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const client = new DynamoDBClient({});
+const client = new DynamoDBClient({
+  region: "us-west-2",
+  accessKeyId: "access_key_id",
+  secretAccessKeyId: "secret_access_key_id",
+  endpoint: "http://localhost:8000",
+});
 
 module.exports = client;
